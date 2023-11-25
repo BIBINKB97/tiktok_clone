5656e35454b19d39b52f124037c8d9969d47325d
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-25 23:38:56 +0530

    created the MVC  architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/view/home.dart';
+import 'package:tiktok_clone/constants/colors.dart';
+import 'package:tiktok_clone/views/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,13 +8,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home:  HomePage(),
+      title: 'TikTok',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: HomePage(),
     );
   }
 }
