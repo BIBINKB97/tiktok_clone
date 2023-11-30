4f124948d25de8217cb59ad04904112af560ef1a
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-30 23:40:59 +0530

    packages imported

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:tiktok_clone/constants/colors.dart';
 import 'package:tiktok_clone/views/screens/auth/sign_up.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,7 +14,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'TikTok',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
